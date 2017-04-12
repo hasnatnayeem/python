91fa71c089fe629075f40f0f8c643fa60bdadbe4
Shakil <sakil58@gmail.com>
2017-04-12 23:03:00 +0600

    find and replace

---
+++ find_and_replace.py
@@ -0,0 +1,21 @@
+import os
+
+location = '/home/shakil/Desktop/abc/'
+file_type = 'php'
+
+text_to_replace = '<? '
+replace_with = '<?php '
+
+files = os.listdir(location)
+for file_name in files:  
+    if file_name.endswith(file_type):
+        file_name =  os.path.realpath(file_name)
+
+        with open(file_name, 'r') as file :
+            content = file.read()
+
+        content = content.replace(, )
+
+        with open(file_name, 'w') as file:
+            file.write(filedata)
+
