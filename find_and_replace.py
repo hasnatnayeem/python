fbb0fbc88359106ba1bb231daa8d13826672717a
Shakil <sakil58@gmail.com>
2017-04-21 21:53:14 +0600

    code fix

--- find_and_replace.py
+++ find_and_replace.py
@@ -2,7 +2,6 @@ import os
 
 location = '/home/shakil/Desktop/abc/'
 file_type = 'php'
-
 text_to_replace = '<? '
 replace_with = '<?php '
 
@@ -10,12 +9,9 @@ files = os.listdir(location)
 for file_name in files:  
     if file_name.endswith(file_type):
         file_name =  os.path.realpath(file_name)
-
         with open(file_name, 'r') as file :
             content = file.read()
-
         content = content.replace(text_to_replace, replace_with)
-
         with open(file_name, 'w') as file:
             file.write(filedata)
 
