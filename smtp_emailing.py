8caeb66ea3a90353d516373f09b91fb7c7ca227f
Abul Hasnat Nayeem <nayeem@softbdltd.com>
2018-01-18 17:42:31 +0600

    Reduce whitespace

--- smtp_emailing.py
+++ smtp_emailing.py
@@ -3,9 +3,7 @@ import smtplib, sys
 my_email_address = sys.argv[1] # Receiving the email id and password from command line
 password = sys.argv[2]
 
-
 smtpObject = smtplib.SMTP_SSL('smtp.gmail.com', 465) # Encryption is enabled by default in this port
-
 smtpObject.login(my_email_address, password) 
 
 to_email = "to_email_address@example.com"
