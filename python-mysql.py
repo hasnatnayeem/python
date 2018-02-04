90b90d2d27cc46f2456277a0bbf3c152db1a001b
Abul Hasnat Nayeem <nayeem@softbdltd.com>
2018-02-04 18:06:10 +0600

    Documentation in python-mysql snippet

--- python-mysql.py
+++ python-mysql.py
@@ -7,15 +7,19 @@ password = "1"
 db_name = "nayeem"
 table_name = "Abc"
 
+# Creating connection and cursor object
 conn = pymysql.connect(host = host, port = port, user = user, passwd = password, db = db_name)
 cur = conn.cursor()
 
 sql = "SELECT * FROM " + table_name
 
+# Executing query using cursor object
 cur.execute(sql)
 
-for r in cur:
-    print(r)
+# Iterating over the results
+for row in cur:
+    print(row)
 
+# Closing cursor and connection
 cur.close()
 conn.close()
