56803ac5d916ae1ff20cf3985427f2eb53ff63d1
Abul Hasnat Nayeem <sakil58@gmail.com>
2018-03-25 12:58:02 +0600

    Draw square using turtle

---
+++ turtle_example.py
@@ -0,0 +1,15 @@
+import turtle
+
+def drawSquare():
+    turtle.speed(1)
+    turtle.shape("turtle")
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.exitonclick()
+
+drawSquare()
