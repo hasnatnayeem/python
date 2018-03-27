42bec3f5fcef61b6714fa4b51b0ea047305ae38f
Abul Hasnat Nayeem <sakil58@gmail.com>
2018-03-27 20:10:40 +0600

    Use command lien arguements for image download

--- image-download.py
+++ image-download.py
@@ -1,6 +1,13 @@
 import requests
+import sys
 
-url = "http://localhost/test_image.jpg";
+url = sys.argv[1];
+file_name = sys.argv[2]
+
 res = requests.get(url)
-with open("downloaded_image.jpg", "wb") as f: # wb for writing binary data
+with open(file_name, "wb") as f: # wb for writing binary data
     f.write(res.content) # content is used for retrieving bytes instead of regular strings
+
+
+# Command to run this program
+# python image-download.py http://image-url.com/.... name_of_the_file
