1d2254bbbcb41b1ff6331643079d31443a5de7ed
Abul Hasnat Nayeem <sakil58@gmail.com>
2018-08-09 12:19:56 +0600

    Binary tree inorder traversal

--- data-strctures/binary-tree.py
+++ data-strctures/binary-tree.py
@@ -27,7 +27,13 @@ class Node:
         if self.right:
             self.right.printTree()
 
-    
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+        return res
 
 
 root = Node(5)
@@ -37,4 +43,4 @@ root.insert(6)
 root.insert(2)
 root.insert(1)
 
-root.printTree()
+print(root.inorderTraversal(root))
